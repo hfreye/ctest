26847edbd340c2a024fb7ddf91b571be9af4095e
hfreye <henry@freye.de>
2016-10-24 22:09:59 +0200

    eingabe der Zeichen jetzt doch mit getchar()

--- blatt2/blatt2_2.c
+++ blatt2/blatt2_2.c
@@ -16,27 +16,17 @@ int main(void) {
 			"Dieses Programm liest vier Zeichen und gibt sie dann wieder aus.\n");
 	//Eingabe wie gefordert
 	//Weil das Enter der Eingabe auch ein Zeichen muss, muss man das abfangen
-	//So bekommt man nur das erste und dritte
-	/*
+	//Sonst bekommt man nur das erste und dritte
+
 	printf("Geben Sie bitte ein 1. Zeichen ein: ");
-	z1 = getchar();
+	z1 = getchar(); enter = getchar();
 	printf("Geben Sie bitte ein 2. Zeichen ein: ");
-	z2 = getchar();
+	z2 = getchar(); enter = getchar();
 	printf("Geben Sie bitte ein 3. Zeichen ein: ");
-	z3 = getchar();
+	z3 = getchar(); enter = getchar();
 	printf("Geben Sie bitte ein 4. Zeichen ein: ");
-	z4 = getchar();
-	*/
-	//Kann ich nur mit scanf !???
-	//Eingabe
-	printf("Geben Sie bitte ein 1. Zeichen ein: ");
-	scanf("%c%c", &z1, &enter);
-	printf("Geben Sie bitte ein 2. Zeichen ein: ");
-	scanf("%c%c", &z2, &enter);
-	printf("Geben Sie bitte ein 3. Zeichen ein: ");
-	scanf("%c%c", &z3, &enter);
-	printf("Geben Sie bitte ein 4. Zeichen ein: ");
-	scanf("%c%c", &z4, &enter);
+	z4 = getchar(); enter = getchar();
+
 	//Ausgabe
 	printf("Der ASCII-Code Ihrer Eingabe lautete: %c %c %c %c \n\n",z1,z2,z3,z4);
 	return (0);
